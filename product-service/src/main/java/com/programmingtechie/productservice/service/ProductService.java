007521883c13a4d03ba0c770bd379ab5b47ce58c
wenjiaaa <wjkong@foxmail.com>
2023-05-21 21:34:40 +0800

    finish product-service

---
+++ product-service/src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.programmingtechie.productservice.service;
+
+import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.dto.ProductResponse;
+import com.programmingtechie.productservice.model.Product;
+import com.programmingtechie.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    // 使用Constructor injection（构造函数注入）的方式将数据保存到数据库
+    // 可以用@RequiredArgsConstructor代替
+    // public ProductService(ProductRepository productRepository) {
+    //    this.productRepository = productRepository;
+    // }
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        //return products.stream().map(product -> mapToProductResponse(product)).toList();
+        //可以写成lambda表达式
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
